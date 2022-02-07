ba20136658a369d0eda12553255169c5fae938fd
LivongS <sandalovlev@gmail.com>
2022-02-07 18:36:26 +0300

    Add files via upload

---
+++ 2.4.3.py
@@ -0,0 +1,15 @@
+from webbrowser import Chrome
+from selenium import webdriver
+import time
+
+try:    
+  browser = webdriver.Chrome()
+  browser.implicitly_wait(5)
+  browser.get("http://suninjuly.github.io/wait1.html")
+  browser.find_element_by_css_selector ("#verify").click()
+  message = browser.find_element_by_css_selector("#verify_message")
+  assert "successful" in message.text
+finally:
+
+  time.sleep(5)
+  browser.quit()
