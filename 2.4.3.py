d92dbc27b5a2a083a19b86461f5cb1f71bdf77f2
LivongS <sandalovlev@gmail.com>
2022-03-03 14:39:52 +0300

    from selenium import webdriver from webdriver_manager.chrome import ChromeDriverManager  driver = webdriver.Chrome(ChromeDriverManager().install())

--- 2.4.3.py
+++ 2.4.3.py
@@ -1,9 +1,11 @@
-from webbrowser import Chrome
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 import time
 
 try:    
-  browser = webdriver.Chrome()
+  browser = webdriver.Chrome(ChromeDriverManager().install())
   browser.implicitly_wait(5)
   browser.get("http://suninjuly.github.io/wait1.html")
   browser.find_element_by_css_selector ("#verify").click()
